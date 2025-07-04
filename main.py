82f8dc10478e698460a17cef6601f76da9489a20
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-07-04 13:29:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import numpy as np
 import mmh3 
+
+class EfficientEmbedding:
+  def __init__(self,
+               num_buckets: int= 1024,
+               dim: int = 128,
+               use_quantization:bool = True):
+      """ 
+      Efficient embedding layer useing features hashiing and quantization.
+
+      Args:
+          num_buckets: size of hashing space (replaces vocabulary size)
+          dim: Embedding dimention
