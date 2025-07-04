5839b7f524c2b42c343cc92850556083d34b9c45
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-07-04 14:35:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ class EfficientEmbedding:
   def __init__(self,
                num_buckets: int= 1024,
                dim: int = 128,
-               use_quantization:bool = True):
+               use_quantization: bool = True):
       """ 
       Efficient embedding layer useing features hashiing and quantization.
 
       Args:
           num_buckets: size of hashing space (replaces vocabulary size)
-          dim: Embedding dimention
+          dim: Embedding dimension
+          use_quantization: Enable 8-bit quantizatiion
+      """
+      
